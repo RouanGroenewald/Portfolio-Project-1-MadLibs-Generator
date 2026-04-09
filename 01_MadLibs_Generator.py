b96815eec0b05c1713c9157c704892a15e35ab6e
RouanGroenewald <108080260+RouanGroenewald@users.noreply.github.com>
2026-04-09 09:16:27 -0700

    Add files via upload

---
+++ 01_MadLibs_Generator.py
@@ -0,0 +1,20 @@
+""" 
+Portfolio Project 1
+Date: 04/07/2026
+File Name: 01_MadLibs_Generator.py
+Project Information: Create an interactive script that prompts users for various parts of speech (nouns, verbs, adjectives) and injects them into a pre-defined story template. 
+This project focuses on mastering the inputs() and Print() functions, string concentration, and f-string formatting. 
+By pushing this into GitHub, you establish your first commit and begin your journey of version-tracked development.
+"""
+print("Welcome to MadLibs Generator")
+
+"""
+Resources:
+1. Wikipedia: https://en.wikipedia.org/wiki/Mad_Libs
+2. Book: "Nightfall" is a 1941 science fiction short story by Isaac Asimov: https://s3.us-west-1.wasabisys.com/luminist/EB/A/Asimov%20-%20Nightfall.pdf
+"""
+
+
+"""
+References:
+"""
