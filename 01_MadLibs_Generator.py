576a78135871bb3021a58b94ef49961c1561a72e
RouanGroenewald <108080260+RouanGroenewald@users.noreply.github.com>
2026-04-10 15:07:34 -0700

    Add files via upload

--- 01_MadLibs_Generator.py
+++ 01_MadLibs_Generator.py
@@ -7,14 +7,30 @@ This project focuses on mastering the inputs() and Print() functions, string con
 By pushing this into GitHub, you establish your first commit and begin your journey of version-tracked development.
 """
 print("Welcome to MadLibs Generator")
+print("Answer the following questions")
+
+#Word List
+adjective1 = input("adjective: ")
+verb1 = input("verb (ending in -ing): ") 
+noun1 = input("noun: ")
+adjective2 = input("adjective: ")
+verb2 = input("verb (past tense): " )
+noun2 = input("noun: ")
+
+madlibs = f"Norman and Livvy were {adjective1}, naturally, since {verb1} a {noun1} is always a matter of last-minute delays, so they had to take the only {adjective2} seat in the coach. It was the one toward the front; the one with nothing before it but the seat that faced the wrong way, with its back hard against the front partition. While Norman {verb2} the {noun2} onto the rack, Livvy found herself chafing a little."
+
+print(madlibs)
 
 """
+Original Mad Libs: The Train Journey
+Norman and Livvy were late, naturally, since catching a train is always a matter of last-minute delays, so they had to take the only available seat in the coach. 
+It was the one toward the front; the one with nothing before it but the seat that faced the wrong way, with it's back hard against the front partition. 
+While Norman heaved the suitcase onto the rack, Livvy found herself chafing a little.
+
 Resources:
 1. Wikipedia: https://en.wikipedia.org/wiki/Mad_Libs
 2. Book: "Nightfall" is a 1941 science fiction short story by Isaac Asimov: https://s3.us-west-1.wasabisys.com/luminist/EB/A/Asimov%20-%20Nightfall.pdf
-"""
 
-
-"""
 References:
+Book: "Nightfall" is a 1941 science fiction short story by Isaac Asimov. page 197 
 """
